ef49e18c0fa13df72ed49822d2b655974ed11b98
paragdhali <paragdhali@gmail.com>
2014-07-08 09:38:41 -0700

    first commit

---
+++ application/controllers/welcome.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	public function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+    }
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+	public function test(){
+		$this->load->view('welcome_message');
+	}
+	public function parag(){
+		$this->load->view('themes/test');
+	}
+}
+
